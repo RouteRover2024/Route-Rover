8d41979efad646335d973c30feab5e20fba01963
Manjiri Milind Chavande <cmanjiri1912@gmail.com>
2024-02-13 12:45:56 +0530

    created frontend folder

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import './index.css'
+function App() {
+
+  return (
+    <div className='gradient-bg-welcome'>
+      <div>
+        <h1 className="text-3xl font-bold underline">
+         Route Rover
+        </h1>
+        <button className="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 ...">
+          Save changes
+        </button>
+      </div>
+      <div className='gradient-bg-services '>
+      <h1>Trial alskjdla skhdhlkasjlkfjldhfd</h1>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
