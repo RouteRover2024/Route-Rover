9b1fa924bc47cc05e79ce5008f9f702ddb095196
SatyamVyas <vyas.satyam.04@gmail.com>
2024-02-17 19:50:42 +0530

    ShadCN config done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,51 @@
-import './App.css'
-import './index.css'
-import {Routes,Route} from "react-router-dom";
-import Home from './routes/Home'
+import { Button, buttonVariants } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
 function App() {
 
   return (
-    <div className='gradient-bg-welcome'>
-     <Routes>
-    <Route path="/" element={<Home />}  exact />
-    </Routes>
+    <div className='w-dvw h-dvh flex justify-center items-center gap-4'>
+     <Card className="w-60">
+        <CardHeader>
+          <CardTitle>Hello Shadcn</CardTitle>
+          <CardDescription>Checking Buttons</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p className="flex justify-center items-center gap-4 flex-col">
+            <Button variant="default">Get Started</Button>
+            <Button variant="secondary">Get Started</Button>
+            <Button variant="destructive">Get Started</Button>
+            <Button variant="outline">Get Started</Button>
+          </p>
+        </CardContent>
+        <CardFooter>
+          <p>GG hehe</p>
+        </CardFooter>
+      </Card>
+      <Card className="w-60 light">
+        <CardHeader>
+          <CardTitle>Light Mode Card</CardTitle>
+          <CardDescription>Checking Buttons</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p className="flex justify-center items-center gap-4 flex-col">
+            <Button variant="default">Get Started</Button>
+            <Button variant="secondary">Get Started</Button>
+            <Button variant="destructive">Get Started</Button>
+            <Button variant="outline">Get Started</Button>
+          </p>
+        </CardContent>
+        <CardFooter>
+          <p>GG hehe</p>
+        </CardFooter>
+      </Card>
     </div>
   )
 }
