6a384b35efa379b20cbf40ba2514e6264df11c7e
SatyamVyas <vyas.satyam.04@gmail.com>
2024-02-17 20:17:17 +0530

    Theme Toggle config | Added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Button, buttonVariants } from "@/components/ui/button"
+import { ThemeProvider } from "@/components/theme-provider"
 import {
   Card,
   CardContent,
@@ -7,29 +8,16 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { ModeToggle } from "./components/ui/mode-toggle"
 
 function App() {
 
   return (
     <div className='w-dvw h-dvh flex justify-center items-center gap-4'>
-     <Card className="w-60">
-        <CardHeader>
-          <CardTitle>Hello Shadcn</CardTitle>
-          <CardDescription>Checking Buttons</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p className="flex justify-center items-center gap-4 flex-col">
-            <Button variant="default">Get Started</Button>
-            <Button variant="secondary">Get Started</Button>
-            <Button variant="destructive">Get Started</Button>
-            <Button variant="outline">Get Started</Button>
-          </p>
-        </CardContent>
-        <CardFooter>
-          <p>GG hehe</p>
-        </CardFooter>
-      </Card>
-      <Card className="w-60 light">
+      <ThemeProvider>
+        <ModeToggle></ModeToggle>
+      </ThemeProvider>
+      <Card className="w-60">
         <CardHeader>
           <CardTitle>Light Mode Card</CardTitle>
           <CardDescription>Checking Buttons</CardDescription>
