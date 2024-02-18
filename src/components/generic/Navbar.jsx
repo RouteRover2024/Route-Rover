0e2ab68a0d9b6299e90e52d08484e437b90270c1
SatyamVyas <vyas.satyam.04@gmail.com>
2024-02-19 00:43:44 +0530

    Basic NavBar setup inside LandingPage

--- src/components/generic/Navbar.jsx
+++ src/components/generic/Navbar.jsx
@@ -3,8 +3,14 @@ import { ModeToggle } from "../ui/mode-toggle";
 
 const NavBar = () => {
 	return (
-		<div className="w-dvw h-16 border-b-2 border-border flex flex-row justify-evenly items-center sticky top-0">
-			<section className="leftside"></section>
+		<div className="w-dvw h-16 px-8 flex flex-row justify-between items-center sticky top-0 z-10 backdrop-blur-sm">
+			<section className="leftside">
+				<img
+					src="src\assets\RouteRoverLogoText.svg"
+					alt="logo"
+					className="h-8"
+				/>
+			</section>
 			<section className="centerstuff"></section>
 			<section className="rightside">
 				<ThemeProvider>
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,6 +4,15 @@ function LandingPage() {
 	return (
 		<div>
 			<NavBar></NavBar>
+			<div className="checking h-dvh w-dvw bg-background border-4 border-accent opacity-50 flex items-center justify-center text-4xl text-primary">
+				Section1
+			</div>
+			<div className="checking h-dvh w-dvw bg-background border-4 border-accent opacity-50 flex items-center justify-center text-4xl text-primary">
+				Section2
+			</div>
+			<div className="checking h-dvh w-dvw bg-background border-4 border-accent opacity-50 flex items-center justify-center text-4xl text-primary">
+				Section3
+			</div>
 		</div>
 	);
 }
