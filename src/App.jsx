800cdd59221a968bf06cd4011dd00c2f78e49f7d
SatyamVyas <vyas.satyam.04@gmail.com>
2024-02-18 14:00:57 +0530

    Moved a few files | Started NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,11 @@
-import { Button, buttonVariants } from "@/components/ui/button"
-import { ThemeProvider } from "@/components/theme-provider"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { ModeToggle } from "./components/ui/mode-toggle"
-import { CheckboxWithText } from "./components/checkbox-custom"
+import NavBar from "./components/Navbar"
 
 function App() {
 
   return (
-    <div className='w-dvw h-dvh flex justify-center items-center gap-4'>
-      <ThemeProvider>
-        <ModeToggle></ModeToggle>
-      </ThemeProvider>
-      <Card className="w-60">
-        <CardHeader>
-          <CardTitle>Light Mode Card</CardTitle>
-          <CardDescription>Checking Buttons</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p className="flex justify-center items-center gap-4 flex-col">
-            <Button variant="default">Get Started</Button>
-            <Button variant="secondary">Get Started</Button>
-            <Button variant="destructive">Get Started</Button>
-            <Button variant="outline">Get Started</Button>
-          </p>
-        </CardContent>
-      </Card>
-    <CheckboxWithText></CheckboxWithText>
+    <div>
+      <NavBar>
+      </NavBar>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,14 @@
+import { ThemeProvider } from "./ui/theme-provider"
+import { ModeToggle } from "./ui/mode-toggle"
 
-const Header = () => {
+const NavBar = () => {
   return (
-    <div>Header</div>
+    <div className="w-dvw h-16 border-b-2 border-sky-400 flex flex-row justify-evenly items-center sticky top-0">
+      <ThemeProvider>
+        <ModeToggle></ModeToggle>
+      </ThemeProvider>
+    </div>
   )
 }
 
-export default Header
+export default NavBar
