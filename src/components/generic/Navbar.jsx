2b78724d44e780964f5d13c2adde48a7a3c43716
SatyamVyas <vyas.satyam.04@gmail.com>
2024-02-19 13:22:54 +0530

    Customised Scrollbar

--- src/components/generic/Navbar.jsx
+++ src/components/generic/Navbar.jsx
@@ -3,7 +3,7 @@ import { ModeToggle } from "../ui/mode-toggle";
 
 const NavBar = () => {
 	return (
-		<div className="w-dvw h-16 px-8 flex flex-row justify-between items-center sticky top-0 z-10 backdrop-blur-sm">
+		<div className="w-[var(--full-width)] h-16 px-8 flex flex-row justify-between items-center sticky top-0 z-10 backdrop-blur-sm">
 			<section className="leftside">
 				<img
 					src="src\assets\RouteRoverLogoText.svg"
