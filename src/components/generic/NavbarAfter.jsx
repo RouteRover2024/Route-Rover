27880626d396f381e09ebe6b71ad5a49e245c359
nishakp3005 <121110503+nishakp3005@users.noreply.github.com>
2024-02-21 23:01:26 +0530

    icons added

--- src/components/generic/NavbarAfter.jsx
+++ src/components/generic/NavbarAfter.jsx
@@ -6,27 +6,28 @@ import {
     ArrowRightLeft,
     HelpCircleIcon
 } from "lucide-react";
+import icon from "../../images/RouteRoverLogoText.svg"
 
 const navLinks = [
     {
         name : "Dashboard",
-        icon : "LayoutDashboard"
+        icon : LayoutDashboard
     },
     {
         name : "Activity",
-        icon : "Clock3"
+        icon : Clock3
     },
     {
         name : "Analytic",
-        icon : "BarChart2"
+        icon : BarChart2
     },
     {
         name : "Transactions",
-        icon : "ArrowRightLeft"
+        icon : ArrowRightLeft
     },
     {
         name : "HelpCenter",
-        icon : "HelpCircleIcon"
+        icon : HelpCircleIcon
     }
 ]
 
@@ -35,18 +36,17 @@ function NavbarAfter() {
     <div className='px-10 py-12 flex flex-col border-r-1 w-1/5 h-screen'>
         <div className="logo-div flex space-x-3 items-center">
             <img 
-                src="src\assets\RouteRoverLogoText.svg"
+                src= {icon}
                 alt="logo"
                 className="h-8"
             />
 		</div>
 
         <div className="mt-9 flex flex-col space-y-8">
-            {navLinks.map((item, index) => <div key = {index}>
+            {navLinks.map((item, index) => <div key = {index} className='flex space-x-4'>
                 <item.icon / >
                 <span>{item?.name}</span>
             </div>)}
-
         </div>
     </div>
     )
