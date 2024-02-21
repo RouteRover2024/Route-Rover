d86046d68e076b1f4f91360c19bff590698d2e58
nishakp3005 <121110503+nishakp3005@users.noreply.github.com>
2024-02-21 23:05:17 +0530

    some more changes

--- src/components/generic/NavbarAfter.jsx
+++ src/components/generic/NavbarAfter.jsx
@@ -1,55 +1,53 @@
-import React from 'react'
+import React from "react";
 import {
     LayoutDashboard,
     Clock3,
     BarChart2,
     ArrowRightLeft,
-    HelpCircleIcon
+    HelpCircleIcon,
 } from "lucide-react";
-import icon from "../../images/RouteRoverLogoText.svg"
+import icon from "../../images/RouteRoverLogoText.svg";
 
 const navLinks = [
     {
-        name : "Dashboard",
-        icon : LayoutDashboard
+        name: "Dashboard",
+        icon: LayoutDashboard,
     },
     {
-        name : "Activity",
-        icon : Clock3
+        name: "Activity",
+        icon: Clock3,
     },
     {
-        name : "Analytic",
-        icon : BarChart2
+        name: "Analytic",
+        icon: BarChart2,
     },
     {
-        name : "Transactions",
-        icon : ArrowRightLeft
+        name: "Transactions",
+        icon: ArrowRightLeft,
     },
     {
-        name : "HelpCenter",
-        icon : HelpCircleIcon
-    }
-]
+        name: "HelpCenter",
+        icon: HelpCircleIcon,
+    },
+];
 
 function NavbarAfter() {
     return (
-    <div className='px-10 py-12 flex flex-col border-r-1 w-1/5 h-screen'>
-        <div className="logo-div flex space-x-3 items-center">
-            <img 
-                src= {icon}
-                alt="logo"
-                className="h-8"
-            />
-		</div>
+        <div className="px-10 py-12 flex flex-col border-r-1 w-1/5 h-screen">
+            <div className="logo-div flex space-x-3 items-center">
+                <img src={icon} alt="logo" className="h-8" />
+            </div>
 
-        <div className="mt-9 flex flex-col space-y-8">
-            {navLinks.map((item, index) => <div key = {index} className='flex space-x-4'>
-                <item.icon / >
-                <span>{item?.name}</span>
-            </div>)}
+            <div className="mt-9 flex flex-col space-y-8">
+                {navLinks.map((item, index) => (
+                    <div key={index} className="flex space-x-4">
+                        <item.icon />
+                        <span>{item?.name}</span>
+                    </div>
+                ))}
+            </div>
         </div>
-    </div>
-    )
+    );
 }
 
-export default NavbarAfter
+export default NavbarAfter;
