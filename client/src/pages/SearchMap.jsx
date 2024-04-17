e3013a6f9cee9a847c42f9d32941e4434c6943ee
SatyamVyas <vyas.satyam.04@gmail.com>
2024-04-17 20:30:33 +0530

    Map Design Rework

--- client/src/pages/SearchMap.jsx
+++ client/src/pages/SearchMap.jsx
@@ -14,7 +14,6 @@ import {
 	DirectionsRenderer,
 } from "@react-google-maps/api";
 
-
 const center = { lat: 19.099279618216062, lng: 72.86539675765846 };
 
 const libraries = ["places"];
@@ -67,9 +66,6 @@ function renderVehicleType(vehicleType) {
 			return "Other";
 	}
 }
-
-
-
 
 function SearchMap() {
 	const { isLoaded } = useJsApiLoader({
@@ -172,10 +168,18 @@ function SearchMap() {
 			legs.forEach((leg) => {
 				const steps = leg.steps;
 				steps.forEach((step) => {
-					if (step.transit && step.transit.line && step.transit.line.vehicle) {
+					if (
+						step.transit &&
+						step.transit.line &&
+						step.transit.line.vehicle
+					) {
 						const vehicleType = step.transit.line.vehicle.type;
-						const departureTime = new Date(step.transit.departure_time.value);
-						const arrivalTime = new Date(step.transit.arrival_time.value);
+						const departureTime = new Date(
+							step.transit.departure_time.value
+						);
+						const arrivalTime = new Date(
+							step.transit.arrival_time.value
+						);
 						const instructions = step.instructions;
 						const fare = route.fare;
 						const num_stops = step.transit.num_stops;
@@ -209,85 +213,123 @@ function SearchMap() {
 	}
 
 	return (
-		<div className="pt-6">
+		<div className="mt-6">
 			<div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-				<h1 className="text-3xl font-semibold text-gray-900">New Journey</h1>
+				<h1 className="text-3xl font-semibold text-gray-900">
+					New Journey
+				</h1>
 			</div>
 			{/* Map */}
-			<div className="p-2 m-2 sm:p-4">
-				<GoogleMap
-					center={center}
-					zoom={15}
-					mapContainerStyle={{ width: "100%", height: "425px" }}
-					onLoad={(map) => setMap(map)}
-				>
-					<Marker position={center} />
-					{directionsResponse && (
-						<DirectionsRenderer
-							directions={directionsResponse}
-							routeIndex={selectedRouteIndex}
-						/>
-					)}
-				</GoogleMap>
-			</div>
-			{/* Inputs and Controls */}
-			<div className="p-2 sm:m-4">
-				<div className="py-2 px-2 flex flex-col sm:flex-row items-center flex-wrap w-full">
-					<div className="min-w-[300px]">
-						<input
-							type="text"
-							placeholder="Origin"
-							ref={originRef}
-							className="w-full block rounded-md focus:ring-slate-500 focus:border-slate-500 text-sm px-4 py-2 my-2 border-zinc-300 border-2"
-							onChange={handleOriginInputChange}
-						/>
+			<div className="flex flex-col md:flex-row p-2 md:p-4 gap-4 w-full justify-center mt-4">
+				<div className="rounded-md lg:w-[70%] sm:min-w-[450px] min-w-[325px]">
+					<GoogleMap
+						center={center}
+						zoom={15}
+						mapContainerStyle={{
+							height: "400px",
+							borderRadius: "6px",
+						}}
+						onLoad={(map) => setMap(map)}
+					>
+						<Marker position={center} />
+						{directionsResponse && (
+							<DirectionsRenderer
+								directions={directionsResponse}
+								routeIndex={selectedRouteIndex}
+							/>
+						)}
+					</GoogleMap>
+				</div>
+				{/* Inputs and Controls */}
+				<div className="flex flex-col items-center justify-between sm:gap-4 gap-2 bg-slate-300 rounded-md p-4">
+					<input
+						type="text"
+						placeholder="Origin"
+						ref={originRef}
+						className="w-full block rounded-md focus:ring-slate-500 focus:border-slate-500 text-sm px-4 py-2 my-1 border-zinc-300 border-2 font-bold font-mono"
+						onChange={handleOriginInputChange}
+					/>
+					<input
+						type="text"
+						placeholder="Destination"
+						ref={destinationRef}
+						className="w-full block rounded-md focus:ring-slate-500 focus:border-slate-500 text-sm px-4 py-2 my-1 border-zinc-300 border-2 font-bold font-mono"
+						onChange={handleDestinationInputChange}
+					/>
+					<div className="flex flex-row items-center gap-4 sm:gap-8">
+						<div className="flex flex-row gap-4 items-center">
+							<div className="font-semibold">
+								Select Travel Mode
+							</div>
+							<select
+								value={travelMode}
+								onChange={handleTravelModeChange}
+								className="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:ring-slate-500 focus:border-slate-500 font-medium font-mono"
+							>
+								<option value="DRIVING">Driving</option>
+								<option value="WALKING">Walking</option>
+								<option value="BICYCLING">Cycling</option>
+								<option value="TRANSIT">Transit</option>
+							</select>
+						</div>
 					</div>
-					<div className="min-w-[300px]">
-						<input
-							type="text"
-							placeholder="Destination"
-							ref={destinationRef}
-							className="w-full block rounded-md focus:ring-slate-500 focus:border-slate-500 text-sm px-4 py-2 my-2 border-zinc-300 border-2"
-							onChange={handleDestinationInputChange}
-						/>
+					<div className="flex flex-row justify-evenly items-center w-full sm:mt-auto mt-2 mb-2">
+						<button
+							type="button"
+							onClick={calculateRoute}
+							className="row-span-2 inline-flex items-center px-4 py-2 border border-green-300 shadow-sm text-sm font-medium rounded-md text-white bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+						>
+							Calculate Route
+						</button>
+						<button
+							type="button"
+							onClick={clearRoute}
+							title="Clear Route"
+							className="inline-flex items-center p-2 border border-transparent rounded-full shadow-sm text-white bg-rose-600 hover:bg-rose-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500"
+						>
+							<FaTimes />
+						</button>
+						<button
+							type="button"
+							className="inline-flex items-center p-2 border border-transparent rounded-full shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+							onClick={() => {
+								map.panTo(center);
+								map.setZoom(15);
+							}}
+							title="ReLocate"
+						>
+							<FaLocationArrow />
+						</button>
 					</div>
 				</div>
+			</div>
+			{/* Transit options */}
+			<div className="p-2 sm:mx-4">
 				{/* Route Options */}
-				<div className="flex flex-col sm:flex-row items-center gap-4 sm:gap-8 mt-4">
+				<div className="flex flex-col sm:flex-row items-center gap-4 sm:gap-8 mt-4 px-4 overflow-auto">
 					{routesInfo.map((route, index) => (
 						<div
 							key={index}
-							className={`p-2 border border-gray-300 rounded-md cursor-pointer ${selectedRouteIndex === index
-								? "bg-gray-100"
-								: "hover:bg-gray-100"
-								}`}
+							className={`p-2 border border-gray-300 rounded-md cursor-pointer ${
+								selectedRouteIndex === index
+									? "bg-gray-100"
+									: "hover:bg-gray-100"
+							}`}
 							onClick={() => handleRouteSelect(route.index)}
 						>
 							<p className="font-semibold">Route {index + 1}</p>
 							{travelMode != "TRANSIT" && (
 								<p>Summary: {route.summary}</p>
 							)}
-
 						</div>
 					))}
 				</div>
-				{/* Travel Mode Selector */}
-				<div className="flex flex-row items-center gap-4 sm:gap-8 mt-4">
-					<div className="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500">
-						<FaRoad />
-					</div>
-					<div className="flex flex-row gap-4 items-center">
-						<div className="font-semibold">Select Travel Mode:</div>
-						<select
-							value={travelMode}
-							onChange={handleTravelModeChange}
-							className="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500"
-						>
-							<option value="DRIVING">Driving</option>
-							<option value="WALKING">Walking</option>
-							<option value="BICYCLING">Cycling</option>
-							<option value="TRANSIT">Transit</option>
-						</select>
+				<div className="flex flex-col items-start gap-1 sm:gap-2 p-2 sm:m-4">
+					<div className="font-semibold">
+						Duration:{" "}
+						{routesInfo[selectedRouteIndex]
+							? routesInfo[selectedRouteIndex].duration
+							: ""}
 					</div>
 					<div className="font-semibold">
 						Distance:{" "}
@@ -296,82 +338,85 @@ function SearchMap() {
 							: ""}
 					</div>
 				</div>
-				{/* Other Controls */}
-				<div className="flex flex-row items-center gap-4 sm:gap-8 mt-4">
-					<div className="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500">
-						<FaMapMarkerAlt />
-					</div>
-					<div className="font-semibold">
-						Duration:{" "}
-						{routesInfo[selectedRouteIndex]
-							? routesInfo[selectedRouteIndex].duration
-							: ""}
-					</div>
-					<div className="flex gap-8 justify-evenly">
-						<button
-							type="button"
-							onClick={calculateRoute}
-							className="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500"
-						>
-							Calculate Route
-						</button>
-						<button
-							type="button"
-							onClick={clearRoute}
-							className="inline-flex items-center p-2 border border-transparent rounded-full shadow-sm text-white bg-slate-600 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500"
-						>
-							<FaTimes />
-						</button>
-						<button
-							type="button"
-							className="inline-flex items-center p-2 border border-transparent rounded-full shadow-sm text-white bg-slate-600 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500"
-							onClick={() => {
-								map.panTo(center);
-								map.setZoom(15);
-							}}
-						>
-							<FaLocationArrow />
-						</button>
-					</div>
-				</div>
-			</div>
-			{/* Transit options */}
 
-			<div className="p-2 sm:m-4">
+				<hr className="border-1 border-black my-2" />
+
 				{transitOptions.length > 0 && (
 					<div>
-						<label htmlFor="transit-options" className="font-semibold">
+						<label
+							htmlFor="transit-options"
+							className="font-semibold"
+						>
 							Transit Options:
 						</label>
 						<div
 							id="transit-options"
 							onChange={(e) =>
-								console.log("Selected transit option:", e.target.value)
+								console.log(
+									"Selected transit option:",
+									e.target.value
+								)
 							}
 							className="p-2 sm:m-4"
 						>
 							{transitOptions
-								.sort((a, b) => a.departureTime - b.departureTime) // Sort by departure time
+								.sort(
+									(a, b) => a.departureTime - b.departureTime
+								) // Sort by departure time
 
 								.map((option, index) => (
 									<div key={index}>
-										<p>Departure Time: {option.departureTime.toLocaleString()}</p>
-										<p>Arrival Time: {option.arrivalTime.toLocaleString()}</p>
-										<p>Mode: {renderVehicleType(option.vehicleType)}</p>
-										<p>Instructions: {option.instructions}</p>
+										<p>
+											Departure Time:{" "}
+											{option.departureTime.toLocaleString()}
+										</p>
+										<p>
+											Arrival Time:{" "}
+											{option.arrivalTime.toLocaleString()}
+										</p>
+										<p>
+											Mode:{" "}
+											{renderVehicleType(
+												option.vehicleType
+											)}
+										</p>
+										<p>
+											Instructions: {option.instructions}
+										</p>
 										{option?.fare && option?.num_stops && (
 											<div>
 												<p>Fare: {option.fare.text}</p>
-												<p>Currency: {option.fare.currency}</p>
-												<p>Total Number of stops: {option.num_stops}</p>
+												<p>
+													Currency:{" "}
+													{option.fare.currency}
+												</p>
+												<p>
+													Total Number of stops:{" "}
+													{option.num_stops}
+												</p>
 											</div>
 										)}
 
 										{option?.transitLine.vehicle && (
 											<div>
-												<img src={option.transitLine.vehicle.icon} alt="Vehicle Icon" />
-												<p>Vehicle Type: {option.transitLine.vehicle.name}</p>
-												<p>Vehicle Name: {option.transitLine.name}</p>
+												<img
+													src={
+														option.transitLine
+															.vehicle.icon
+													}
+													alt="Vehicle Icon"
+												/>
+												<p>
+													Vehicle Type:{" "}
+													{
+														option.transitLine
+															.vehicle.name
+													}
+												</p>
+												<p>
+													Vehicle Name:{" "}
+													{option.transitLine.name}
+												</p>
 											</div>
 										)}
 									</div>
@@ -385,7 +430,11 @@ function SearchMap() {
 						<div>
 							{transitOptions
 								.reduce((acc, option) => {
-									const existingVehicleType = acc.find(item => item.vehicleType === option.vehicleType);								
+									const existingVehicleType = acc.find(
+										(item) =>
+											item.vehicleType ===
+											option.vehicleType
+									);
 									if (!existingVehicleType) {
 										acc.push(option);
 										return acc;
@@ -395,17 +444,40 @@ function SearchMap() {
 								.map((option, index) => (
 									<div key={index}>
 										<div>
-											<p>Transit Agency Name: {option?.transitLine.agencies[0].name}</p>
-											<p>Transit Agency Phone: {option?.transitLine.agencies[0].phone}</p>
-											<p>Transit Agency URL: <a href={option?.transitLine.agencies[0].url}>{option?.transitLine.agencies[0].url}</a></p>
-
+											<p>
+												Transit Agency Name:{" "}
+												{
+													option?.transitLine
+														.agencies[0].name
+												}
+											</p>
+											<p>
+												Transit Agency Phone:{" "}
+												{
+													option?.transitLine
+														.agencies[0].phone
+												}
+											</p>
+											<p>
+												Transit Agency URL:{" "}
+												<a
+													href={
+														option?.transitLine
+															.agencies[0].url
+													}
+												>
+													{
+														option?.transitLine
+															.agencies[0].url
+													}
+												</a>
+											</p>
 										</div>
 									</div>
 								))}
 						</div>
 					)}
 				</div>
-
 			</div>
 		</div>
 	);
